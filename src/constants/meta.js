bed6ddc2ece3df322fb18d563094adf5ba0e00c4
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2020-10-07 16:39:15 +0100

    add meta that has info about manifestr metadata and schema.org and social Media

---
+++ src/constants/meta.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteTitle: "HMA", // Navigation and Site Title
+  siteTitleAlt: "This a little portfolio and blog for Hamza Miloud Amar", // Alternative Site title for SEO
+  siteTitleShort: "hma", // short_name for manifest
+  siteUrl: process.env.ROOT_URL || "https://hamzaamar.com", // Domain of your site. No trailing slash!
+  lang: "en", // Language Tag on <html> element
+  pathPrefix: "/",
+  siteLogo: "images/logo.png", // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription: `Hi am Hamza Miloud Amar this is my portfolio and blog that   describe myself 
+                    and can help you find same useful articles and show you how to create some
+                    projects from scratch about web development`,
+  minibio: `
+      <strong>Hi am Hamza Miloud Amar</strong> am JavaScript  and web developer 
+      i like to work with javascript and css `,
+  author: "Hamza Miloud Amar", // Author for schemaORGJSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: "@kentcdodds", // Twitter Username
+  ogSiteName: "Kent C. Dodds", // Facebook Site Name
+  ogLanguage: "en_US",
+
+  // Manifest and Progress color
+  themeColor: "#4147DC",
+  backgroundColor: "#231C42",
+
+  // Social component
+  twitter: "https://twitter.com/kentcdodds/",
+  twitterHandle: "@kentcdodds",
+  github: "https://github.com/kentcdodds/",
+  linkedin: "https://www.linkedin.com/in/kentcdodds/",
+  rss: "https://kentcdodds.com/blog/rss.xml",
+}
