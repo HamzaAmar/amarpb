30dd55aa8b64f6e0fd730fca4b968509576ca967
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2020-10-15 14:57:02 +0100

    add layout to posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby" // highlight-line
 import styled from "@emotion/styled"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import SEO from "../components/Seo"
+import Layout from "../components/layout"
 
 const Container = styled.div`
   padding: 3rem;
@@ -13,14 +14,14 @@ export default function BlogPost({ data }) {
   const { frontmatter, body } = data.mdx
   // highlight-end
   return (
-    <>
+    <Layout>
       <SEO isBlogPost frontmatter={frontmatter} />
       <Container>
         <h1>hello from blogs</h1>
         <h1>{frontmatter.title}</h1>
         <MDXRenderer>{body}</MDXRenderer>
       </Container>
-    </>
+    </Layout>
   )
 }
 
