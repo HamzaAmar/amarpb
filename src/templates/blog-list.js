c18100d47786b25f62f72c0d1dd17661ba56dbc5
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2020-10-19 13:01:52 +0100

    add tags  template and leave blogs from pages and add to template to work with pagination

---
+++ src/templates/blog-list.js
@@ -0,0 +1,84 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import styled from "@emotion/styled"
+import { FaGrinWink, FaNetworkWired } from "react-icons/fa"
+import { DiCode } from "react-icons/di"
+
+import SEO from "../components/seo"
+import Article from "../components/article"
+import Layout from "../components/layout"
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: auto;
+  grid-gap: 1.5rem;
+  padding: 1rem 2rem;
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+  @media (max-width: 600px) {
+    padding: 1rem;
+  }
+`
+
+const navBarData = [
+  { id: "about", Icon: <FaNetworkWired size={30} /> },
+  { id: "services", Icon: <FaGrinWink size={30} /> },
+  { id: "projects", Icon: <DiCode size={30} /> },
+]
+
+const Blog = ({ data }) => {
+  console.log(data)
+  return (
+    <Layout navBarData={navBarData}>
+      <SEO title="blog Page" />
+
+      <Container>
+        {data &&
+          data.blogs &&
+          data.blogs.edges &&
+          data.blogs.edges.map(({ node }) => {
+            const { frontmatter, fields } = node
+
+            return (
+              <Article
+                key={frontmatter.title}
+                article={{ ...frontmatter, ...fields }}
+              />
+            )
+          })}
+      </Container>
+    </Layout>
+  )
+}
+export const query = graphql`
+  query($skip: Int!, $limit: Int!) {
+    blogs: allMdx(
+      filter: { fileAbsolutePath: { regex: "//data/blogs//" } }
+      sort: { order: ASC, fields: frontmatter___date }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            tags
+            keywords
+            image
+            description
+            author
+            category
+          }
+        }
+      }
+    }
+  }
+`
+export default Blog
