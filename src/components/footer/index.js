d4e1c59b3f0a9c39ad4072207f3f9b498143455e
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2020-10-19 13:05:48 +0100

    add footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,13 +1,33 @@
 import React from "react"
-import {Container,Contact } from './style'
+import Text from "../field/Text"
+import { Container, Contact, CopyRight, Subscription } from "./style"
+import Social from "../social"
 
 const Footer = () => {
   return (
     <Container>
-      <div>Hello world</div>
-      <Contact>Hello world</Contact>
-      <Contact>Hello world</Contact>
-      <div><p>© 2020 Hamza Miloud Amar. All Rights Reserved</p></div>
+      <Contact>
+        <h3>Thank you</h3>
+        <p>
+          Hello thank you for the visit if you like my website and you want to
+          see more staff about me please follow me in social media
+        </p>
+        <Social />
+      </Contact>
+
+      <Subscription>
+        <h3>Join the Newsletter</h3>
+        <p>Please subscribe to our web site to see all new stuff</p>
+        <Text
+          name="subscribe"
+          buttonInside={true}
+          placeholder="yourname@gmail.com"
+          buttonText="Subscribe"
+        />
+      </Subscription>
+      <CopyRight>
+        <p>© 2020 Hamza Miloud Amar. All Rights Reserved</p>
+      </CopyRight>
     </Container>
   )
 }
--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -1,22 +1,60 @@
 import styled from "@emotion/styled"
 
 export const Container = styled.footer`
-  display:grid;
-  grid-template-columns: 2fr 1fr 1fr;
-  grid-template-rows:2fr 1fr;
-  grid-auto-rows:1fr;
-
-  @media (max-width:800px){
-    grid-template-columns: 2fr 1fr;
-    grid-template-rows:1fr 1fr;
-    padding-bottom:5rem;
-
-  }
-  @media (max-width:600px){
-    grid-template-columns: 1fr;
+  display: grid;
+  grid-template-areas: "contact subscription" "copyright copyright";
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  margin-top: 1rem;
+  box-shadow: var(--color-shadow);
+  background: var(--color-background-main);
+  @media (max-width: 600px) {
+    grid-template-areas: "contact" "subscription" "copyright";
   }
 `
 
+export const Subscription = styled.div`
+  grid-area: subscription;
+  padding: 4rem 0.2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background: var(--color-background-main);
+  filter: brightness(1.5);
+  h3 {
+    font-weight: 700;
+  }
+  label {
+    display: block;
+  }
+  input {
+    width: 100%;
+    height: 2rem;
+    background: var(--color-background-dark);
+    outline: none;
+  }
+`
 export const Contact = styled.div`
-  display:grid;
+  grid-area: contact;
+  background: var(--color-background-main);
+  backdrop-filter: brightness(0.5);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  p {
+    width: 80%;
+    margin: 1rem auto;
+  }
+`
+
+export const CopyRight = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 0.9rem;
+  grid-area: copyright;
+  background: var(--color-background-light);
+  padding: 1rem;
+  /* width: 100%; */
 `
